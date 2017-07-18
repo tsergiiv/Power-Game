9c503001d8eb75ab86dda4befaf7d52cd7cf1374
tsergiiv <tsergiiv@gmail.com>
2017-07-18 20:24:31 +0300

    Keyboard

---
+++ Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour {
+
+	public Text amount;
+
+	public void _click(int number) {
+		int start = int.Parse(amount.text);
+		amount.text = (start*10 + number).ToString();
+	}
+}
